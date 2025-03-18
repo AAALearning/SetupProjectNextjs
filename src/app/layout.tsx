126f4d4af0f6079f074f71aefda64de097f9fde0
hieucuopbien123 <hieucuopbien123@gmail.com>
2025-03-18 08:22:35 +0700

    fix: add docs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import { ReactScan } from "@/components/common/ReactScan";
+import Script from "next/script";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,9 +8,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <ReactScan/> */}
       <head>
         <link rel="preload" as="image" href="../favicon.ico" />
+        {/* Phải dùng Script tránh lỗi */}
+        <Script crossOrigin="anonymous" src="//unpkg.com/react-scan/dist/auto.global.js"></Script>
       </head>
       <body>{children}</body>
     </html>
